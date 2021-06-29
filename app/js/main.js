f16936b72d783b38de6a6f708e6d3d5df79868f2
Royal_Aver <royal_aver@rambler.ru>
2021-06-29 13:19:55 +0300

    added footer and fixed header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
 $(function () {
-	$('.slider').slick({
 
-	})
+	$(window).on("scroll", function () {
+		if ($(window).scrollTop() > 50) {
+			$('.header').addClass('header--scroll')
+		}
+		else
+			$('.header').removeClass('header--scroll')
+	});
 
 });
