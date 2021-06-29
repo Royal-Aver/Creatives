7ce39a3edac9dbb7b78be469e1b41b2bdea05981
Royal_Aver <royal_aver@rambler.ru>
2021-06-29 16:35:00 +0300

    responsive header and top

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,31 @@ $(function () {
 			$('.header').removeClass('header--scroll')
 	});
 
+	$(window).on("scroll", function () {
+		if ($(window).scrollTop() > 50) {
+			$('.header__inner').addClass('header__inner--padding')
+		}
+		else
+			$('.header__inner').removeClass('header__inner--padding')
+	});
+
+	$(window).on("scroll", function () {
+		if ($(window).scrollTop() > 50) {
+			$('.menu__link').addClass('menu__link--padding')
+		}
+		else
+			$('.menu__link').removeClass('menu__link--padding')
+	});
+
+	$(".menu__link, .top__link").on("click", function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({ scrollTop: top }, 1500);
+	});
+
+	$('.burger').on('click', function () {
+		$('.menu__list').toggleClass('menu__list--active');
+	});
+
 });
